693b948ed0895eb2d10b573b59e3b1f3e1115d85
root on puppetmaster <root@puppetmaster.vagrant.local>
2014-07-11 15:24:03 +0000

    primo commit

---
+++ manifests/init.pp
@@ -0,0 +1,34 @@
+class dhttpd(
+  $path,
+  $ensure ='present',
+  $port   ='8089',
+  $bind   ='0.0.0.0',
+) {
+
+  if ($ensure=='present') {
+    package { 'dhttpd': ensure => 'present'; }
+
+    file { '/etc/default/dhttpd':
+      ensure  => 'present',
+      owner   => root,
+      group   => admin,
+      mode    => '0664',
+      content => template('dhttpd/dhttpd'),
+      require => Package['dhttpd'],
+      notify  => Service['dhttpd'],
+    }
+
+    service { 'dhttpd':
+      ensure      => running,
+      hasrestart  => true,
+      hasstatus   => false,
+      status      => 'ps -C dhttpd',
+      require     => File['/etc/default/dhttpd'],
+    }
+  }
+  else {
+
+    package { 'dhttpd': ensure => 'purged'; }
+    file { '/etc/default/dhttpd': ensure  => 'absent', }
+  }
+}
